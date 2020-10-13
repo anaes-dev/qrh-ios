c28afbeef83c706de5a56d1ef1b11f71f6814b53
anaesdev <anaesdev@gmail.com>
2020-10-13 22:47:58 +0100

    Parsing JSON

---
+++ qrh/GuidelineDetail.swift
@@ -0,0 +1,97 @@
+//
+//  GuidelineDetail.swift
+//  qrh
+//
+//  Created by user185107 on 10/13/20.
+//
+
+import UIKit
+import SwiftyJSON
+
+private let reuseIdentifier = "Cell"
+
+
+class GuidelineDetail: UICollectionViewController {
+    
+    var passedCode = String()
+    var passedTitle = String()
+    var guidelineDetail = [[String : AnyObject]]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+               
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        
+        
+//        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+//        let cell1 = "Cell1"
+//        collectionView.register(GuidelineCell1.self, forCellWithReuseIdentifier: cell1)
+        fetchGuidelineDetail()
+        print(passedCode)
+        self.navigationItem.title = passedTitle
+
+        // Do any additional setup after loading the view.
+    }
+    
+   
+
+    func fetchGuidelineDetail() {
+        let url = Bundle.main.url(forResource: passedCode, withExtension: "json")!
+        let fileContents = try! Data(contentsOf: url)
+        let jsonData1 = try! JSON(data: fileContents)
+        let jsonData2 = jsonData1["DetailContent"].arrayObject
+        self.guidelineDetail = jsonData2 as! [[String : AnyObject]]
+        self.collectionView?.reloadData()
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return guidelineDetail.count
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GuidelineListCell", for: indexPath) as! GuidelineListCell
+        let iP = guidelineDetail[indexPath.row]
+        print(iP["main"])
+        cell.MAIN.text = iP["main"] as? String
+   
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
--- qrh/GuidelinesController.swift
+++ qrh/GuidelinesController.swift
@@ -8,36 +8,56 @@
 import UIKit
 import SwiftyJSON
 
-class GuidelinesController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class GuidelinesController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+    
     
     var guidelines = [[String : AnyObject]]()
-       
+    var filteredGuidelines = [[String : AnyObject]]()
+    var passCode: String?
+    var passTitle: String?
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchGuidelineList()
+        filteredGuidelines = guidelines
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+      return 1
+   }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return guidelines.count
-    }
+            return filteredGuidelines.count
+        }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "GuidelinesCell") as? GuidelinesCell else { return UITableViewCell() }
-        let iP = guidelines[indexPath.row]
-        let versionText = "v.\(iP["version"]!)"
-        cell.title.text = iP["title"] as? String
-        cell.code.text = iP["code"] as? String
-        cell.version.text = versionText
-        return cell
+            let iP = filteredGuidelines[indexPath.row]
+            let versionText = "v.\(iP["version"]!)"
+            cell.title.text = iP["title"] as? String
+            cell.code.text = iP["code"] as? String
+            cell.version.text = versionText
+            return cell
     }
+      
+    
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let iP = guidelines[indexPath.row]
-        let code = iP["code"] as? String
-        print(code!)
+        let iP = filteredGuidelines[indexPath.row]
+        passCode = iP["code"] as? String
+        passTitle = iP["title"] as? String
+        self.performSegue(withIdentifier: "LoadDetail", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? GuidelineDetail {
+            destination.passedCode = passCode!
+            destination.passedTitle = passTitle!
+        }
+           
     }
     
     func fetchGuidelineList() {
@@ -48,8 +68,11 @@ class GuidelinesController: UIViewController, UITableViewDataSource, UITableView
         self.guidelines = jsonData2 as! [[String : AnyObject]]
         tableView.reloadData()
     }
-    
+     
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        let searchText = searchText.lowercased()
+        print(searchText)
 
-
-
+        tableView.reloadData()
+    }
 }
