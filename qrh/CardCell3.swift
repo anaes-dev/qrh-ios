0a1986f22eeeb63ca5f78bdcdee9d116ee1a9885
Matt <mattroundphone@gmail.com>
2020-10-17 13:19:43 +0100

    Cleanup, remove SwiftyJSON, add card types

---
+++ qrh/CardCell3.swift
@@ -0,0 +1,28 @@
+//
+//  CardCell3.swift
+//  qrh
+//
+//  Created by user185107 on 10/15/20.
+//
+
+import UIKit
+
+class CardCell3: UITableViewCell {
+
+    @IBOutlet weak var main: UILabel!
+    @IBOutlet weak var sub: UILabel!
+    @IBOutlet weak var step: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
