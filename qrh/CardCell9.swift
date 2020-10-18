081748ff22e6330b146b091bf4146621a2ef4939
Matt <mattroundphone@gmail.com>
2020-10-18 09:42:40 +0100

    ActiveLabel

---
+++ qrh/CardCell9.swift
@@ -0,0 +1,26 @@
+//
+//  CardCell9.swift
+//  qrh
+//
+//  Created by user185107 on 10/17/20.
+//
+
+import UIKit
+import ActiveLabel
+
+class CardCell9: UITableViewCell {
+
+    @IBOutlet weak var sub: ActiveLabel!
+    @IBOutlet weak var main: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
